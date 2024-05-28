fdff998b12bffcafee533b27b188c38b5f0acfc2
Steven Nataniel <stevnathar@gmail.com>
2024-05-28 22:47:26 +0700

    first ui design

---
+++ app/src/main/java/com/capstone/cleanup/ui/detail/DetailReportActivity.kt
@@ -0,0 +1,21 @@
+package com.capstone.cleanup.ui.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.cleanup.R
+
+class DetailReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_report)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
